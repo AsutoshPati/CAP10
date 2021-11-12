4ed44395105c48ec1d3aee0646c0d86cec545824
AsutoshPati <asutoshpati64@gmail.com>
2021-11-12 22:50:15 +0530

    Files extension modified

---
+++ cap10.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author           - Asutosh Pati
+ * Created On       - 12 November 2021
+ * Last Updated On  - 12 November 2021
+ */
+
+#include "cap10.h"
+
+void add2nums(int num1, num2){
+  return num1+num2;
+}
--- cap10.cpp.ino
+++
@@ -1,5 +0,0 @@
-#include "cap10.h"
-
-void add2nums(int num1, num2){
-  return num1+num2;
-}
